2b40ebdf3b0c9fd00acb976ad226907ce0ef392e
Piotr Zolnierek <pz@anixe.pl>
2010-12-11 19:17:07 +0100

    [FIXED] path

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ export install_dir=$(cd `mktemp -d platform-install.XXXX`; pwd)
 git clone git://github.com/pzol/platform-install.git
 # wget --no-check-certificate -nv https://github.com/pzol/platform-install/tarball/master
 # tar xzf master
-cd pzol-platform-install
+cd platform-install
 . ./install.sh $install_dir && run
 cd ..
 echo "Installation files are in $install_dir, please remove manually"
