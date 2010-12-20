ddcf2a55bd2a869448ea32c1f2d4931aee4de783
Piotr Zolnierek <pz@anixe.pl>
2010-12-20 21:51:49 +0100

    [FIXED]

--- babushka-deps/xenia.rb
+++ babushka-deps/xenia.rb
@@ -3,7 +3,7 @@ dep 'xenia', :for => :linux  do
 	met? { File.directory? "/opt/xenia" }
 	meet {
 		git "ssh://xenia@194.213.22.181/var/git/xenia" do |path|
-			shell "cap deploy:setup -s domain=127.0.0.1 && cap deploy -s domain=127.0.0.1"
+			shell "cap deploy:setup -s domain=`$USER`@127.0.0.1 && cap deploy -s domain=`$USER`@127.0.0.1"
 		end
 	}
 end
