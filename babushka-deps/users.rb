a02dc50435d62ad754c78ab2191a07cc02262cf7
Piotr Zolnierek <pz@anixe.pl>
2010-12-28 10:32:23 +0100

    [ADDED] userkey meta which adds a ida_rsa.pub key pasted into the command line as a key for a user

--- babushka-deps/users.rb
+++ babushka-deps/users.rb
@@ -5,6 +5,23 @@ meta 'user' do
 	}
 end
 
+meta 'userkey' do
+	template {
+		def pub_key; var(:pub_key); end
+		def ssh_path; "/home/#{basename}/.ssh"; end
+		def authorized_keys_path; File.join(ssh_path, "authorized_keys"; end
+		def contains_key?; grep(/#{pub_key}/, authorized_keys); end
+		def create_ssh_path; shell "mkdir -p #{ssh_path}"; end
+		def append_pub_key; shell "echo '#{pub_key} >> #{authorized_keys_path}'"; end
+
+		met? { contains_key? }
+		meet {
+			create_ssh_path unless File.exist?(ssh_path)
+			append_pub_key	unless contains_key?
+		}	
+	}
+end
+
 meta 'group' do
 	template {
 		met? { grep /^#{basename}\:/, '/etc/group' }
