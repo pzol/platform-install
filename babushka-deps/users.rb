7fcea08d128e00e91030f90ce69b380a1d5b7333
Piotr Zolnierek <pz@anixe.pl>
2010-12-28 11:06:43 +0100

    [FIXED] permissions for .ssh dir

--- babushka-deps/users.rb
+++ babushka-deps/users.rb
@@ -10,13 +10,16 @@ meta 'userkey' do
 		def pub_key; var(:pub_key); end
 		def ssh_path; "/home/#{basename}/.ssh"; end
 		def authorized_keys_path; File.join(ssh_path, "authorized_keys"); end
+		def permitted?; stat(ssh_path).mode.to_s(8) == "40755"; end
+		def fix_permissions; shell "chmod 755 -R #{ssh_path}"; end
 		def contains_key?; shell "grep '#{pub_key}' #{authorized_keys_path}"; end
-		def create_ssh_path; shell "mkdir -p #{ssh_path}"; end
+		def create_ssh_path; shell "mkdir -p -m 755 #{ssh_path}"; end
 		def append_pub_key; shell "echo '#{pub_key}' >> #{authorized_keys_path} && chown #{basename}:#{basename} #{authorized_keys_path}"; end
 
-		met? { contains_key? }
+		met? { contains_key? && permitted? }
 		meet {
 			create_ssh_path unless File.directory?(ssh_path)
+			fix_permissions unless permitted?
 			append_pub_key	unless contains_key?
 		}	
 	}
