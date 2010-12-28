e2d9b055c3cad4556470cf1838df5dac3df04033
Piotr Zolnierek <pz@anixe.pl>
2010-12-28 20:30:19 +0100

    [ADDED] deploy dirs

--- babushka-deps/xenia.rb
+++ babushka-deps/xenia.rb
@@ -4,7 +4,7 @@ dep 'xenia' do
 	met? { File.directory? "/opt/xenia/current" }
 	meet {
 		git "git://git.anixe.pl/xenia.git" do |path|
-			log_shell "Capistrano setup", "cap deploy:setup  HOSTS=127.0.0.1"
+			log_shell "Capistrano setup", "cap deploy:setup HOSTS=127.0.0.1"
 			log_shell "Capistrano deploy", "cap deploy HOSTS=127.0.0.1"
 		end
 	}
@@ -21,7 +21,7 @@ end
 
 dep 'xenia.dirs' do
 	requires 'deploy.user', 'deploy.userkey', 'deploy.group'
-	dirs '/var/data/xenia', '/var/log/xenia', '/var/data/export', '/opt/xenia'
+	dirs '/var/data/xenia', '/var/log/xenia', '/var/data/export', '/opt/xenia', '/opt/xenia/releases', '/opt/xenia/shared', '/opt/xenia/shared/system',  '/opt/xenia/shared/log',  '/opt/xenia/shared/pids'
 	user 'deploy'
 	group 'deploy'
 	mask '2774'			# drwrwxs--
