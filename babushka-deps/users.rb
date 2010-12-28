b49ab095d74352bad851baa8856f9cef0ce278e0
Piotr Zolnierek <pz@anixe.pl>
2010-12-28 22:17:19 +0100

    [FIXED] permissions for .ssh dir are required to be 700

--- babushka-deps/users.rb
+++ babushka-deps/users.rb
@@ -13,12 +13,12 @@ meta 'userkey' do
 		def uid; @uid ||= shell("grep '^#{basename}' /etc/passwd").split(":")[2].to_i; end
 		def gid; @gid ||= shell("grep '^#{basename}' /etc/group").split(":")[2].to_i; end
 		def stat(dir); File.lstat(dir); end
-		def permitted?; File.lstat(ssh_path).mode.to_s(8) == "40755"; end
+		def permitted?; File.lstat(ssh_path).mode.to_s(8) == "40700"; end
 		def ownership?; stat(ssh_path).uid == uid && stat(ssh_path).gid == gid; end
-		def fix_permissions; shell "chmod 755 -R #{ssh_path}"; end
+		def fix_permissions; shell "chmod 700 -R #{ssh_path}"; end
 		def fix_ownership; shell "chown #{basename}:#{basename} -R #{ssh_path}"; end
 		def contains_key?; shell "grep '#{pub_key}' #{authorized_keys_path}"; end
-		def create_ssh_path; shell "mkdir -p -m 755 #{ssh_path}"; end
+		def create_ssh_path; shell "mkdir -p -m 700 #{ssh_path}"; end
 		def append_pub_key; shell "echo '#{pub_key}' >> #{authorized_keys_path} && chown #{basename}:#{basename} #{authorized_keys_path}"; end
 
 		met? { contains_key? && permitted? && ownership? }
