19b9aa8001fe2ad3ed5c384a9c25e80e927fd6bc
Piotr Zolnierek <pz@anixe.pl>
2010-12-28 19:07:33 +0100

    [ADDED] ownership check & fix

--- babushka-deps/users.rb
+++ babushka-deps/users.rb
@@ -1,7 +1,7 @@
 meta 'user' do
 	template {
 		met? { grep /^#{basename}\:/, '/etc/passwd' }
-		meet { sudo "useradd -g deploy --create-home --system -s /bin/bash #{basename}" }
+		meet { sudo "useradd --user-group --create-home --system -s /bin/bash #{basename}" }
 	}
 end
 
@@ -10,16 +10,22 @@ meta 'userkey' do
 		def pub_key; var(:pub_key); end
 		def ssh_path; "/home/#{basename}/.ssh"; end
 		def authorized_keys_path; File.join(ssh_path, "authorized_keys"); end
+		def uid; @uid ||= shell("grep '^#{basename}' /etc/passwd").split(":")[2].to_i; end
+		def gid; @gid ||= shell("grep '^#{basename}' /etc/group").split(":")[2].to_i; end
+		def stat(dir); File.lstat(dir); end
 		def permitted?; File.lstat(ssh_path).mode.to_s(8) == "40755"; end
+		def ownership?; stat(ssh_path).uid == uid && stat(ssh_path).gid == gid; end
 		def fix_permissions; shell "chmod 755 -R #{ssh_path}"; end
+		def fix_ownership; shell "chown #{basename}:#{basename} -R #{ssh_path}"; end
 		def contains_key?; shell "grep '#{pub_key}' #{authorized_keys_path}"; end
 		def create_ssh_path; shell "mkdir -p -m 755 #{ssh_path}"; end
 		def append_pub_key; shell "echo '#{pub_key}' >> #{authorized_keys_path} && chown #{basename}:#{basename} #{authorized_keys_path}"; end
 
-		met? { contains_key? && permitted? }
+		met? { contains_key? && permitted? && ownership? }
 		meet {
 			create_ssh_path unless File.directory?(ssh_path)
 			fix_permissions unless permitted?
+			fix_ownership unless ownership?
 			append_pub_key	unless contains_key?
 		}	
 	}
