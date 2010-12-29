54dbeca1fd27ed1c52b8b978979836ccf1ec7ba0
Piotr Zolnierek <pz@anixe.pl>
2010-12-29 18:15:41 +0100

    [REFACTOR] to have atomic deps

--- babushka-deps/platform.rb
+++ babushka-deps/platform.rb
@@ -4,19 +4,16 @@ end
 
 dep 'deploy.user'
 
-dep 'root has deploy key' do
-	def generate_key; shell "mkdir -p  /root/.ssh && ssh-keygen -N '' -f /root/.ssh/id_rsa -t rsa -q"; end
-	def copy_root_key; shell "cat /root/.ssh/id_rsa.pub >> /home/deploy/.ssh/authorized_keys"; end
-	def root_key_exist?; File.exist?("/root/.ssh/id_rsa") && File.exist?("/root/.ssh/id_rsa.pub"); end
-	def deploy_has_root_key?; shell "grep \"`cat /root/.ssh/id_rsa.pub`\" /home/deploy/.ssh/authorized_keys"; end
+dep 'root has deploy key' do 	# we need this to be able to deploy to localhost
+	requires 'deploy.user', 'root key exists'
+	met? { shell "grep \"`cat /root/.ssh/id_rsa.pub`\" /home/deploy/.ssh/authorized_keys" }
+	meet { shell "cat /root/.ssh/id_rsa.pub >> /home/deploy/.ssh/authorized_keys"	}
+end
 
-	met? { root_key_exist? && deploy_has_root_key? }
-	meet {
-		generate_key unless root_key_exist?
-		copy_root_key unless deploy_has_root_key?
-	}
+dep 'root key exists' do
+	met? { File.exist?("/root/.ssh/id_rsa") && File.exist?("/root/.ssh/id_rsa.pub") }
+	meet { shell "mkdir -p  /root/.ssh && ssh-keygen -N '' -f /root/.ssh/id_rsa -t rsa -q" }
 end
 
 dep 'deploy.group'
-
 dep 'deploy.userkey'
