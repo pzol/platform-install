1b2316f328c239c25cf368b12b999f28a104da0f
Piotr Zolnierek <pz@anixe.pl>
2010-12-29 08:09:27 +0100

    [FIXED] permissions for .ssh

--- babushka-deps/users.rb
+++ babushka-deps/users.rb
@@ -14,12 +14,12 @@ meta 'userkey' do
 		def uid; @uid ||= shell("grep '^#{basename}' /etc/passwd").split(":")[2].to_i; end
 		def gid; @gid ||= shell("grep '^#{basename}' /etc/group").split(":")[2].to_i; end
 		def stat(dir); File.lstat(dir); end
-		def permitted?; File.lstat(ssh_path).mode.to_s(8) == "40644"; end
+		def permitted?; File.lstat(ssh_path).mode.to_s(8) == "40755"; end
 		def ownership?; stat(home_path).uid == uid && stat(home_path).gid == gid; end
-		def fix_permissions; shell "chmod 644 -R #{ssh_path}"; end
+		def fix_permissions; shell "chmod 755 -R #{ssh_path}"; end
 		def fix_ownership; shell "chown #{basename}:#{basename} -R #{home_path}"; end
 		def contains_key?; shell "grep '#{pub_key}' #{authorized_keys_path}"; end
-		def create_ssh_path; shell "mkdir -p -m 644 #{ssh_path}"; end
+		def create_ssh_path; shell "mkdir -p -m 755 #{ssh_path}"; end
 		def append_pub_key; shell "echo '#{pub_key}' >> #{authorized_keys_path} && chown #{basename}:#{basename} #{authorized_keys_path}"; end
 
 		met? { contains_key? && permitted? && ownership? }
