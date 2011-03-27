7f062bc4f0af1bc0b8ab78b36207c62fa2a2650c
Piotr Zolnierek <pz@anixe.pl>
2011-03-27 07:45:15 +0200

    qfe

--- babushka-deps/god.rb
+++ babushka-deps/god.rb
@@ -33,7 +33,7 @@ end
 
 dep 'started.god' do
   requires 'init_d.god', 'conf.god'
-	met? { shell("ps aux | grep 'bin/god' | grep -v grep | wc -l")["1"], :sudo => true }
+	met? { shell("ps aux | grep 'bin/god' | grep -v grep | wc -l", :sudo => true)["1"] }
 	meet { shell "/etc/init.d/god start", :sudo => true }
 end
 
