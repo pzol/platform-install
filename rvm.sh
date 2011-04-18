e51d93de3719e7274273057d55199bb1eeacb8a1
Piotr Zolnierek <pz@anixe.pl>
2011-04-18 12:43:55 +0200

    [ADDED] installing a specific version of rvm

--- rvm.sh
+++ rvm.sh
@@ -33,8 +33,9 @@ function install_rvm {
 		apt-get -q update
 		echo "Downloading and installing system pre-requisites"
 		apt-get install -qq -y libruby1.8 zlib1g-dev libssl-dev libreadline5-dev build-essential libxslt-dev libxml2-dev curl git-core
-		curl -# -L https://rvm.beginrescueend.com/install/rvm > $rvm_install_path/rvm-install
-		bash -l < $rvm_install_path/rvm-install
+		curl -# -L https://rvm.beginrescueend.com/install/rvm -o $rvm_install_path/rvm-installer
+    chmod +x rvm-installer
+		bash -l < $rvm_install_path/rvm-installer 1.6.3
 
 		usermod -a -G rvm $USER
 		rvm_install_to_profile
