6f2bc38ee8c876be1fb9241edcd30aa4f0f0fe16
Sebastian Styka <ss@anixe.pl>
2011-06-07 15:05:42 +0200

    [ADDED] creating authorized_keys file for user deploy

--- babushka-deps/users.rb
+++ babushka-deps/users.rb
@@ -28,11 +28,13 @@ meta 'userkey' do
     def contains_own_key?; File.exists?(File.join(ssh_path, "id_rsa.pub")); end
     def create_own_key; shell "su #{basename}"; shell "ssh-keygen -t rsa -q"; shell "exit" end
 		def create_ssh_path; shell "mkdir -p -m 755 #{ssh_path}"; end
+    def create_authorized_keys_path; shell "touch #{authorized_keys_path}"; end
 		def append_pub_key; shell "echo '#{pub_key}' >> #{authorized_keys_path} && chown #{basename}:#{basename} #{authorized_keys_path}"; end
 
-		met? { contains_key? && permitted? && ownership? && ssh_ownership? && contains_own_key?}
+		met? { File.directory?(ssh_path) && File.exist?(authorized_keys_path) && contains_key? && permitted? && ownership? && ssh_ownership? && contains_own_key?}
 		meet {
 			create_ssh_path unless File.directory?(ssh_path)
+      create_authorized_keys_path unless File.exist?(authorized_keys_path)
 			fix_permissions unless permitted?
 			fix_ownership unless ownership?
       fix_ssh_ownership unless ssh_ownership?
