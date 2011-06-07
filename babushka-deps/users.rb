d9a4d4c6dbfdab5a37ec4abf62cd354420e473c8
Sebastian Styka <ss@anixe.pl>
2011-06-07 15:33:04 +0200

    [FIXED] removed creation of deploy's public key as it didn't work

--- babushka-deps/users.rb
+++ babushka-deps/users.rb
@@ -26,19 +26,18 @@ meta 'userkey' do
     def fix_ssh_ownership; shell "chown #{basename}:#{basename} -R #{ssh_path}"; end
 		def contains_key?; shell "grep '#{pub_key}' #{authorized_keys_path}"; end
     def contains_own_key?; File.exist?(File.join(ssh_path, "id_rsa.pub")); end
-    def create_own_key; shell "sudo su #{basename}"; shell "ssh-keygen -t rsa -q"; shell "exit" end
+    #def create_own_key; shell "su #{basename}"; shell "ssh-keygen -t rsa -q"; shell "exit" end
 		def create_ssh_path; shell "mkdir -p -m 755 #{ssh_path}"; end
     def create_authorized_keys_path; shell "touch #{authorized_keys_path}"; end
 		def append_pub_key; shell "echo '#{pub_key}' >> #{authorized_keys_path} && chown #{basename}:#{basename} #{authorized_keys_path}"; end
 
-		met? { File.directory?(ssh_path) && File.exist?(authorized_keys_path) && contains_key? && permitted? && ownership? && ssh_ownership? && contains_own_key?}
+		met? { File.directory?(ssh_path) && File.exist?(authorized_keys_path) && contains_key? && permitted? && ownership? && ssh_ownership?}
 		meet {
 			create_ssh_path unless File.directory?(ssh_path)
       create_authorized_keys_path unless File.exist?(authorized_keys_path)
 			fix_permissions unless permitted?
 			fix_ownership unless ownership?
       fix_ssh_ownership unless ssh_ownership?
-      create_own_key unless contains_own_key?
 			append_pub_key unless contains_key?
 		}	
 	}
