db3596df321d6747cf53c6ca7267d30c12f3cd4c
Michał Wikliński <mwi@anixe.pl>
2011-06-15 11:35:33 +0200

    [ADDED] redis init.d script

--- babushka-deps/redis.rb
+++ babushka-deps/redis.rb
@@ -1,5 +1,18 @@
+dep 'init_d.redis' do
+	met? { File.exists? "/etc/init.d/redis" }
+	meet { 
+		render_erb "redis/init.d.erb", :to => '/etc/init.d/redis', :perms => '755', :sudo => true
+		sudo 'update-rc.d redis defaults'
+	}
+end
+
+dep 'started.redis' do
+	met? { shell("ps aux | grep redis") }
+	meet { shell "/etc/init.d/redis-server start", :sudo => true }
+end
+
 dep 'redis' do
-  requires 'redis-server.managed'
+  requires 'redis-server.managed', 'init_d.redis', 'started.redis'
 end
 
 dep 'redis-server.managed' do
