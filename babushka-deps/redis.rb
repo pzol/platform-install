9399fd8f9b001c12c029ff55e91d248c84da1432
Michał Wikliński <mwi@anixe.pl>
2011-06-15 12:00:18 +0200

    [ADDED] Changing permissions of redis logs directory

--- babushka-deps/redis.rb
+++ babushka-deps/redis.rb
@@ -1,4 +1,4 @@
-dep 'init_d.redis' do
+dep 'redis.init_d' do
 	met? { File.exists? "/etc/init.d/redis" }
 	meet { 
 		render_erb "redis/init.d.erb", :to => '/etc/init.d/redis', :perms => '755', :sudo => true
@@ -6,13 +6,20 @@ dep 'init_d.redis' do
 	}
 end
 
-dep 'started.redis' do
+dep 'redis.started' do
 	met? { shell("ps aux | grep redis") }
 	meet { shell "/etc/init.d/redis-server start", :sudo => true }
 end
 
+dep 'redis.dirs' do
+	dirs '/var/log/redis'
+	user 'deploy'
+	group 'deploy'
+	mask '0775'
+end
+
 dep 'redis' do
-  requires 'redis-server.managed', 'init_d.redis', 'started.redis'
+  requires 'redis-server.managed', 'redis.init_d', 'dirs.redis', 'redis.started'
 end
 
 dep 'redis-server.managed' do
