570796de32361157a8b5c5506b678024c570cfca
Wojciech Maryszczak <wojciech.maryszczak@anixe.pl>
2011-06-17 07:34:12 +0200

    [ADDED] castor services directory dependencies

--- babushka-deps/castor-services.rb
+++ babushka-deps/castor-services.rb
@@ -1,5 +1,16 @@
 dep 'castor-services' do
+	requres 'castor-services.dirs', 'castor-services.freetds'
+end
+
+dep 'castor-services.freetds' do
 	shell "apt-get -y install freetds-dev", :sudo => true
 end
 
+dep 'castor-services.dirs' do
+	requires 'deploy.user', 'deploy.userkey', 'deploy.group'
+	dirs '/var/log/castor_services', '/opt/castor_services', '/opt/castor_services/releases', '/opt/castor_services/shared', '/opt/castor_services/shared/system',  '/opt/castor_services/shared/log',  '/opt/castor_services/shared/pids'
+	user 'deploy'
+	group 'deploy'
+	mask '774'			# drwrwxs--
+end
 
